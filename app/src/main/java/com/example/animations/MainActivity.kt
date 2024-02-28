3bfeed89816608a3ebdba82d2f8caf711bb18db9
Joatan Carlos Farias Feitosa <joatancarlos@gmail.com>
2024-02-28 12:08:01 -0300

    initial commit

---
+++ app/src/main/java/com/example/animations/MainActivity.kt
@@ -0,0 +1,76 @@
+package com.example.animations
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.animations.ui.theme.AnimationsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AnimationsTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    AnimationApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun AnimationApp(modifier: Modifier = Modifier) {
+    AnimateAsStateDemo(modifier)
+}
+
+@Composable
+fun AnimateAsStateDemo(modifier: Modifier) {
+    var blue by remember { mutableStateOf(true) }
+    val color by animateColorAsState(if (blue) Color.Blue else Color.Green)
+
+    Column(modifier) {
+        Button(onClick = { blue = !blue }) {
+            Text(text = "CHANGE COLOR")
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Box(
+            modifier = Modifier
+            .size(128.dp)
+            .background(color = color)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AnimationAppPreview() {
+    AnimationsTheme {
+        AnimationApp(modifier = Modifier.fillMaxSize().padding(16.dp))
+    }
+}
