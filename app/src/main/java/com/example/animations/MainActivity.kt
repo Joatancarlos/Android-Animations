d7c06fb37ca802e5f7499dae10fc6dd4a21d6bdc
Joatan Carlos Farias Feitosa <joatancarlos@gmail.com>
2024-02-28 14:55:35 -0300

    Animação de CrossFade

--- app/src/main/java/com/example/animations/MainActivity.kt
+++ app/src/main/java/com/example/animations/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.Crossfade
 import androidx.compose.animation.animateColor
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.animateContentSize
@@ -20,6 +21,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -30,6 +32,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
@@ -62,11 +65,10 @@ fun AnimationApp(modifier: Modifier = Modifier) {
             .padding(16.dp)) {
         AnimateAsStateDemo(modifier)
         Spacer(modifier = modifier.height(16.dp))
-        UpdateTransitionDemo(modifier)
+        CrossFadeDemo(modifier)
         Spacer(modifier = modifier.height(16.dp))
         AnimateVisibilityDemo(modifier)
         Spacer(modifier = modifier.height(16.dp))
-
         AnimateContentSizeDemo(modifier)
     }
 }
@@ -182,6 +184,40 @@ fun AnimateContentSizeDemo(modifier: Modifier) {
                 modifier = Modifier.padding(16.dp),
                 maxLines = if (expanded) Int.MAX_VALUE else 2
             )
+        }
+    }
+}
+
+private enum class DemoScene {
+    Text, Icon
+}
+
+@Composable
+fun CrossFadeDemo(modifier: Modifier) {
+    var scene by remember { mutableStateOf(DemoScene.Text) }
+
+    Column() {
+        Button(onClick = {
+            scene = when (scene) {
+                DemoScene.Text -> DemoScene.Icon
+                DemoScene.Icon -> DemoScene.Text
+            }
+        }) {
+            Text(text = "TOGGLE")
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Crossfade(label = "", targetState = scene) { scene ->
+            when (scene) {
+                DemoScene.Text -> Text(
+                    text = "Phone",
+                    fontSize = 32.sp
+                )
+                DemoScene.Icon -> Icon(
+                    painter = painterResource(id = R.drawable.baseline_local_phone_24),
+                    contentDescription = "Phone",
+                    modifier = Modifier.size(48.dp)
+                )
+            }
         }
     }
 }
